cd688a4a4a87b335050b95d71d2b6e53e3f73509
xjackhammered <hammerofjack7@gmail.com>
2025-04-28 12:59:29 +0600

    registered models and created superuser

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category, Thoughts, Comment
+
+admin.site.register(Category)
+admin.site.register(Thoughts)
+admin.site.register(Comment)
+
 
 # Register your models here.
+
+
