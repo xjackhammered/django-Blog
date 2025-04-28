1f98d29a8e9cb2b58d05b41beab6e04c711424ca
xjackhammered <hammerofjack7@gmail.com>
2025-04-28 16:29:59 +0600

    added templates, worked on urls.py and views.py

--- post/urls.py
+++ post/urls.py
@@ -1,6 +1,7 @@
-from django.contrib import admin
 from django.urls import path
+from . import views 
 
 urlpatterns = [
-
+    path("home/", views.thoughts_list, name="thoughts"),
+    path("thought/<int:id>", views.single_thought, name="single"),
 ]
--- post/views.py
+++ post/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Thoughts, Comment
 
 # Create your views here.
 
+def thoughts_list(request):
+    thoughts = Thoughts.objects.all()
+    return render(request, "post/index.html", {'thoughts': thoughts})
+
+def single_thought(request,id):
+    thought = get_object_or_404(Thoughts, id=id)
+    comments = Comment.objects.filter(thoughts=thought)
+    return render(request, "post/single.html", {'thought':thought, 'comments':comments})
