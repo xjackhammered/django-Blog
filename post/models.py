c5edd65e8229943ccd0b3d7f663a062b01f4f4d1
xjackhammered <hammerofjack7@gmail.com>
2025-04-28 13:15:04 +0600

    configured the models (urls and views next)

--- post/models.py
+++ post/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
-    def __init__(self):
+    def __str__(self):
         return self.name
 
 class Thoughts(models.Model):
@@ -14,7 +14,7 @@ class Thoughts(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
-    def __init__(self):
+    def __str__(self):
         return self.title
 
 class Comment(models.Model):
@@ -23,5 +23,5 @@ class Comment(models.Model):
     content = models.TextField(max_length=200)
     author = models.CharField(max_length=100)
 
-    def __init__(self):
+    def __str__(self):
         return f"Comment by {self.author}"
